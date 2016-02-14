7a48ef3105adc914430fd474f9e37a74ad63ea8a
alirafiq <ali.almaas@gmail.com>
2016-02-14 19:01:52 +0500

    server

---
+++ server.js
@@ -0,0 +1,37 @@
+var net = require('net');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var logs = mongoose.model('log', { logs: String ,dated:Number});
+
+var HOST = process.env.OPENSHIFT_NODEJS_IP;
+var PORT =  process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        var log = new logs({ log: data,dated : new Date().getTime() });
+log.save(function (err) {
+   });
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
